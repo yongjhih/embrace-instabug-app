39355f3dd2cfdc6a287feab8e825c2c2ddc3ccb2
Andrew Chen <yongjhih@gmail.com>
2019-10-16 18:37:18 -0700

    Integrate Embrace

--- app/src/main/java/com/github/yongjhih/embrace/instabug/App.kt
+++ app/src/main/java/com/github/yongjhih/embrace/instabug/App.kt
@@ -1,2 +1,11 @@
 package com.github.yongjhih.embrace.instabug
 
+import android.app.Application
+import io.embrace.android.embracesdk.Embrace
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Embrace.getInstance().start(this)
+    }
+}
