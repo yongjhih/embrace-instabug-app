4d7651a16df70a174eee4ecc179035562e5aebfb
Andrew Chen <yongjhih@gmail.com>
2019-10-16 18:42:15 -0700

    Integrate Instabug

--- app/src/main/java/com/github/yongjhih/embrace/instabug/App.kt
+++ app/src/main/java/com/github/yongjhih/embrace/instabug/App.kt
@@ -1,11 +1,16 @@
 package com.github.yongjhih.embrace.instabug
 
 import android.app.Application
+import com.instabug.library.Instabug
+import com.instabug.library.invocation.InstabugInvocationEvent
 import io.embrace.android.embracesdk.Embrace
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Embrace.getInstance().start(this)
+        Instabug.Builder(this, "5f22ae9a98bd5e8e8e35cde6d0f8bb83")
+            .setInvocationEvents(InstabugInvocationEvent.SHAKE, InstabugInvocationEvent.SCREENSHOT)
+            .build()
     }
 }
