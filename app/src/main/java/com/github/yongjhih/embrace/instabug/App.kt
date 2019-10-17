e51727d8cf6e29b8bcf2bafe8108d1a6e29b8a55
Andrew Chen <yongjhih@gmail.com>
2019-10-16 18:59:22 -0700

    Disable embrace

--- app/src/main/java/com/github/yongjhih/embrace/instabug/App.kt
+++ app/src/main/java/com/github/yongjhih/embrace/instabug/App.kt
@@ -8,7 +8,7 @@ import io.embrace.android.embracesdk.Embrace
 class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
-        Embrace.getInstance().start(this)
+        //Embrace.getInstance().start(this)
         Instabug.Builder(this, "5f22ae9a98bd5e8e8e35cde6d0f8bb83")
             .setInvocationEvents(InstabugInvocationEvent.SHAKE, InstabugInvocationEvent.SCREENSHOT)
             .build()
